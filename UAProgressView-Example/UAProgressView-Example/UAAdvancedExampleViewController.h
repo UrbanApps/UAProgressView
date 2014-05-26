89d0760cd340cbb4b3c12398f3de3d540f204d8d
Matt Coneybeare <coneybeare@gmail.com>
2014-05-26 11:17:08 -0400

    Example Image

---
+++ UAProgressView-Example/UAProgressView-Example/UAAdvancedExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  UAAdvancedExampleViewController.h
+//  UAProgressView-Example
+//
+//  Created by Matt Coneybeare on 5/26/14.
+//  Copyright (c) 2014 Urban Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UAAdvancedExampleViewController : UIViewController
+
+@end
