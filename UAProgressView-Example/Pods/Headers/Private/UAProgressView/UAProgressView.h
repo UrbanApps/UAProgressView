26567db28571bb63c59b32c47ecafbaeae5a3674
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-12-29 13:33:33 +0900

    Update example Pods cache

---
+++ UAProgressView-Example/Pods/Headers/Private/UAProgressView/UAProgressView.h
@@ -0,0 +1,1 @@
+../../../../../UAProgressView.h
---
+++ UAProgressView-Example/Pods/Target Support Files/UAProgressView/UAProgressView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UAProgressView : NSObject
+@end
+@implementation PodsDummy_UAProgressView
+@end
